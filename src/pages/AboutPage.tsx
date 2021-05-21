34f84ae8e9ab7a97eadef0377241d95645c512fc
jdim21 <kdim218@gmail.com>
2021-05-20 22:47:42 -0400

    about v2

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -15,13 +15,15 @@ export default function AboutPage() {
             <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '4rem', width: '100%' }}>
                 <h2 style={{ color: '#bc13fe', textAlign: 'center', paddingTop: '1rem', width: '50%' }}>
                     In the future, we will use the STUD token to grant access to charts and other utilities
-                    as we release them, so grab a few if you enjoy the site!
+                    as we release them, so grab a few if you enjoy the site! Next, we would like to expose
+                    our price data via API and automatically add all Serum custom markets. Stay tuned!
                 </h2>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', paddingTop: '4rem', width: '100%' }}>
                 <h2 style={{ color: '#bc13fe', textAlign: 'center', paddingTop: '1rem', width: '50%' }}>
-                    Follow us on Socials (find them across the footer bar of the site) for more info. Reach
-                    out with any suggestions or bug reports to solanatools@protonmail.com.
+                    Contact us to have your market listed! Follow us on Socials (find them across the 
+                    footer bar of the site) for more info. Reach out with any suggestions or bug reports 
+                    to solanatools@protonmail.com.
                 </h2>
             </div>
         </>
